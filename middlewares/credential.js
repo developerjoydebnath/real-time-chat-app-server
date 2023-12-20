f5977ff73f52b8071e7043506aed9f0cf4a0b7a4
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-12-21 02:31:08 +0600

    update 4

--- middlewares/credential.js
+++ middlewares/credential.js
@@ -5,9 +5,13 @@ const config = {
 const credentials = (req, res, next) => {
     const listedOrigins = config.ORIGIN.split(',');
     const origin = req.headers.origin;
+
     if (listedOrigins.indexOf(origin) !== -1) {
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Max-Age', '1800');
+        res.setHeader('Access-Control-Allow-Headers', 'content-type');
+        res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
     }
     next();
 };
